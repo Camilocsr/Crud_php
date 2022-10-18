5ad8cadc9226a29505098dad71c2e3e35f6504a8
Camilocsr <esmunred@gmail.com>
2022-10-17 22:40:32 -0500

    inicio de crud con php y mysql

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php
+include ("inc/head.php");
+?>
+<body>
+  <?php
+    include ("inc/navbar.php");
+
+  ?>
+  <script src="js/ajax.js"></script>
+  <script src="js/codigoDeBulma.js"></script>
+</body>
+</html>
